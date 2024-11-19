e1e724fcda4679ec1acede28edaedabf19e773ef
Mohammad <mnikmard2004@gmail.com>
2024-11-19 23:08:33 +0330

    add print menu util

--- utils.py
+++ utils.py
@@ -1,1 +1,10 @@
-
+def print_menu():
+    """Prints the menu options."""
+    print("\nMenu:")
+    print("1. Add Task")
+    print("2. View Tasks")
+    print("3. Delete Task")
+    print("4. Delete All Tasks")
+    print("5. Mark Task as Complete")
+    print("6. Search for a Task")
+    print("7. Exit")
