60b900816913056562f3638cae182297af018aa4
amirhosseinkh12 <mnikmard1344@gmail.com>
2024-11-19 23:17:58 +0330

    add main function

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print("Hello World")
+from module import ToDoManager
+from utils import print_menu, get_user_input
+from constants import WELCOME_MESSAGE, INVALID_CHOICE
+
+def main():
+    print(WELCOME_MESSAGE)
+    todo_manager = ToDoManager()
+
+if __name__ == "__main__":
+    main()
+
