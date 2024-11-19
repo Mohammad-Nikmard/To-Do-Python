e8cb70d2a6b78c1833ff5bf515e8d09cfb5adf24
Mohammad <mnikmard2004@gmail.com>
2024-11-19 23:13:07 +0330

    add ability to add a task

--- module.py
+++ module.py
@@ -1,6 +1,11 @@
 class ToDoManager:
     def __init__(self):
         self.tasks = [] 
+        
+    def add_task(self, task):
+        """Adds a task to the to-do list."""
+        self.tasks.append({"name": task, "completed": False})
+        print(f"Task added: {task}")
 
 
 
